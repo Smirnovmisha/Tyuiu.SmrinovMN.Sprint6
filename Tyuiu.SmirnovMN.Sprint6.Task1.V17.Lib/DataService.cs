5657cdc9701bf93b32a3145f52ba21b5ea2ddd8a
Tyuiu.SmirnovMN@outlook.com <Tyuiu.SmirnovMN@outlook.com>
2023-12-03 19:51:21 +0500

    Task1

---
+++ Tyuiu.SmirnovMN.Sprint6.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SmirnovMN.Sprint6.Task1.V17.Lib
+{
+    public class DataService
+    {
+    }
+}
